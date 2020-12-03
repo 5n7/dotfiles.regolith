b2262cf049599b58aa30e904b8942e0ecabe3cc8
Shunta Komatsu <shuntak217@gmail.com>
2020-12-03 12:21:26 +0900

    feat: support multiple monitors

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -1,16 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-## Add this to your wm startup file.
-
-# Terminate already running bar instances
 killall -q polybar
 killall -q zscroll
 
-# Wait until the processes have been shut down
 while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
-# polybar main -c ~/.config/polybar/config1.ini &
-# polybar main -c ~/.config/polybar/config2.ini &
-# polybar main -c ~/.config/polybar/config3.ini &
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+done
